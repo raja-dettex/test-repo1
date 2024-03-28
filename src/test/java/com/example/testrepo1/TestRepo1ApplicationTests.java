6b9467956341b9088f7de3140a2beb111b3c7411
raja-dettex <rk6091039@gmail.com>
2024-03-28 11:51:00 +0530

    test commit

---
+++ src/test/java/com/example/testrepo1/TestRepo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testrepo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
